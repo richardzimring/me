33e5b718e320ba60a889054f32b3b3815d73c62c
Richard Zimring <zimringrichard@gmail.com>
2026-05-22 21:33:03 -0500

    feat(library): add and curate entries

--- src/data/library.ts
+++ src/data/library.ts
@@ -11,19 +11,25 @@ export interface LibraryItem {
  * Order in which kinds are rendered on /library/.
  * Items within each kind preserve their declaration order below.
  */
-export const kindOrder: LibraryItemKind[] = ['video', 'doc', 'essay', 'book', 'podcast', 'movie'];
+export const kindOrder: LibraryItemKind[] = ['video', 'doc', 'essay', 'book', 'movie', 'podcast'];
 
 /** Plural label used as the section heading per kind. */
 export const kindLabel: Record<LibraryItemKind, string> = {
   book: 'books',
   movie: 'movies',
-  doc: 'docs',
+  doc: 'documentaries',
   podcast: 'podcasts',
   essay: 'essays',
   video: 'videos',
 };
 
 export const library: LibraryItem[] = [
+  {
+    title: 'Google DeepMind: The Podcast',
+    kind: 'podcast',
+    by: 'Hannah Fry',
+    href: 'https://deepmind.google/the-podcast/',
+  },
   {
     title: 'Final Thoughts on Free Will',
     kind: 'podcast',
@@ -34,7 +40,7 @@ export const library: LibraryItem[] = [
     title: 'Punished by Rewards',
     kind: 'book',
     by: 'Alfie Kohn',
-    href: 'https://www.alfiekohn.org/punished-by-rewards/',
+    href: 'https://www.alfiekohn.org/punished-rewards/',
   },
   {
     title: 'Thinking, Fast and Slow',
@@ -67,12 +73,6 @@ export const library: LibraryItem[] = [
     href: 'https://en.wikipedia.org/wiki/The_Lord_of_the_Rings',
   },
   {
-    title: 'Harry Potter',
-    kind: 'book',
-    by: 'J. K. Rowling',
-    href: 'https://en.wikipedia.org/wiki/Harry_Potter',
-  },
-  {
     title: 'The Big Short',
     kind: 'movie',
     href: 'https://en.wikipedia.org/wiki/The_Big_Short_(film)',
@@ -83,16 +83,15 @@ export const library: LibraryItem[] = [
     href: 'https://en.wikipedia.org/wiki/Office_Space',
   },
   {
+    title: 'Whiplash',
+    kind: 'movie',
+    href: 'https://en.wikipedia.org/wiki/Whiplash_(2014_film)',
+  },
+  {
     title: 'Planet Earth II',
     kind: 'doc',
     by: 'David Attenborough',
-    href: 'https://www.bbc.co.uk/programmes/b036gq7',
-  },
-  {
-    title: 'introducing kendrick lamar to an italian poet',
-    kind: 'video',
-    by: 'GUS',
-    href: 'https://www.youtube.com/watch?v=q5dqCeNEIFU',
+    href: 'https://en.wikipedia.org/wiki/Planet_Earth_II',
   },
   {
     title: 'The Intelligence Curse',
@@ -113,6 +112,36 @@ export const library: LibraryItem[] = [
     href: 'https://situational-awareness.ai/',
   },
   {
+    title: 'Los Alamos From Below',
+    kind: 'video',
+    by: 'Richard Feynman',
+    href: 'https://www.youtube.com/watch?v=uY-u1qyRM5w',
+  },
+  {
+    title: 'Fun to Imagine',
+    kind: 'video',
+    by: 'Richard Feynman',
+    href: 'https://www.youtube.com/watch?v=P1ww1IXRfTA',
+  },
+  {
+    title: 'Day In The Life of The #1 BBQ In Texas',
+    kind: 'doc',
+    by: 'Alvin Zhou',
+    href: 'https://www.youtube.com/watch?v=1hvPTqswJn4',
+  },
+  {
+    title: 'introducing kendrick lamar to an italian poet',
+    kind: 'video',
+    by: 'GUS',
+    href: 'https://www.youtube.com/watch?v=q5dqCeNEIFU',
+  },
+  {
+    title: 'Facing Eviction',
+    kind: 'doc',
+    by: 'FRONTLINE',
+    href: 'https://www.youtube.com/watch?v=XBVshMTOF08',
+  },
+  {
     title: 'You Are Two',
     kind: 'video',
     by: 'CGP Grey',
@@ -125,9 +154,21 @@ export const library: LibraryItem[] = [
     href: 'https://www.3blue1brown.com/topics/neural-networks',
   },
   {
-    title: 'Neural Networks: Zero to Hero',
+    title: 'makemore',
     kind: 'video',
     by: 'Andrej Karpathy',
     href: 'https://www.youtube.com/playlist?list=PLAqhIrjkxbuWI23v9cThsA9GvCAUhRvKZ',
+  },
+  {
+    title: 'Free To Choose',
+    kind: 'doc',
+    by: 'Milton Friedman',
+    href: 'https://www.freetochoosenetwork.org/programs/free_to_choose/index_80.php',
+  },
+  {
+    title: 'Free Solo',
+    kind: 'doc',
+    by: 'Alex Honnold',
+    href: 'https://en.wikipedia.org/wiki/Free_Solo',
   },
 ];
